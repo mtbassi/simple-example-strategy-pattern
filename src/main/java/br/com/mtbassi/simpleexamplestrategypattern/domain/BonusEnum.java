fc05abc1f7e6a19a7b49f07b06f6e6ecef5b534e
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-29 22:19:51 -0300

    feat: implement bonus enum and method for calculating salary adjustment

---
+++ src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/BonusEnum.java
@@ -0,0 +1,40 @@
+package br.com.mtbassi.simpleexamplestrategypattern.domain;
+
+import java.math.BigDecimal;
+
+public enum BonusEnum {
+
+    JUNIOR {
+        @Override
+        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.2)));
+        }
+    },
+    PLENO {
+        @Override
+        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.3)));
+        }
+    },
+    SENIOR {
+        @Override
+        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.4)));
+        }
+    },
+    TECHNICAL_LIDER {
+        @Override
+        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.5)));
+        }
+    },
+    MANAGER {
+        @Override
+        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.6)));
+        }
+    };
+
+    public abstract BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage);
+
+}
