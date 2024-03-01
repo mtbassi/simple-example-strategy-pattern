1cd900259088b700faf7a58ff7aff94f0705d7e4
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-29 22:07:47 -0300

    initial config

---
+++ src/main/java/br/com/mtbassi/simpleexamplestrategypattern/SimpleExampleStrategyPatternApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtbassi.simpleexamplestrategypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExampleStrategyPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleExampleStrategyPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mtbassi/simpleexamplestrategypattern/SimpleExampleStrategyPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtbassi.simpleexamplestrategypattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleExampleStrategyPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
