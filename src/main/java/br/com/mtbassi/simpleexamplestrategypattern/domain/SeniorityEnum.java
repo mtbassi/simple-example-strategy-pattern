72e06cc8a24f75c97d1d9b1bd03d91dd10237b9c
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-29 22:30:21 -0300

    fix: calculating only the bonus

--- src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/SeniorityEnum.java
+++ src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/SeniorityEnum.java
@@ -6,35 +6,34 @@ public enum SeniorityEnum {
 
     JUNIOR {
         @Override
-        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+        public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
             return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.2)));
         }
     },
     PLENO {
         @Override
-        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+        public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
             return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.3)));
         }
     },
     SENIOR {
         @Override
-        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+        public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
             return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.4)));
         }
     },
     TECHNICAL_LIDER {
         @Override
-        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+        public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
             return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.5)));
         }
     },
     MANAGER {
         @Override
-        public BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage) {
+        public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
             return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.6)));
         }
     };
-
-    public abstract BigDecimal calculatesSalaryAdjustment(BigDecimal currentWage);
+    public abstract BigDecimal calculateSalaryBonus(BigDecimal currentWage);
 
 }
