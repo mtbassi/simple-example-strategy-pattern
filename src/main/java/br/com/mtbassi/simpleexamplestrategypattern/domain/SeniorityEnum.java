7981b70f4f7a5583b179a2d7f5f63c5c72d330be
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-29 23:21:17 -0300

    fix: fix bonus calculation and increase percentage

--- src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/SeniorityEnum.java
+++ src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/SeniorityEnum.java
@@ -1,39 +1,48 @@
 package br.com.mtbassi.simpleexamplestrategypattern.domain;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public enum SeniorityEnum {
 
     JUNIOR {
         @Override
         public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
-            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.2)));
+            return currentWage.multiply(BigDecimal.valueOf(0.2));
         }
     },
     PLENO {
         @Override
         public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
-            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.3)));
+            return currentWage.multiply(BigDecimal.valueOf(0.3));
         }
     },
     SENIOR {
         @Override
         public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
-            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.4)));
+            return currentWage.multiply(BigDecimal.valueOf(0.4));
         }
     },
     TECHNICAL_LIDER {
         @Override
         public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
-            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.5)));
+            return currentWage.multiply(BigDecimal.valueOf(0.5));
         }
     },
     MANAGER {
         @Override
         public BigDecimal calculateSalaryBonus(BigDecimal currentWage) {
-            return currentWage.add(currentWage.multiply(BigDecimal.valueOf(0.6)));
+            return currentWage.multiply(BigDecimal.valueOf(0.6));
         }
     };
+
     public abstract BigDecimal calculateSalaryBonus(BigDecimal currentWage);
+
+    public static BigDecimal calculateBonusPercentage(BigDecimal currentWage, BigDecimal salaryIncrease) {
+        return salaryIncrease.subtract(currentWage)
+                .divide(currentWage, 2, RoundingMode.HALF_EVEN)
+                .multiply(BigDecimal.valueOf(100))
+                .setScale(0, RoundingMode.HALF_EVEN);
+    }
 
 }
