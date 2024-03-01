f0d03d7487e3f25ee07ad1e0b0d0d84f496a8461
Matheus Bassi <bassi.matheus@outlook.com>
2024-03-01 00:52:18 -0300

    feat: adds other implementation strategies

---
+++ src/main/java/br/com/mtbassi/simpleexamplestrategypattern/strategy/impl/SeniorityTechLeadStrategy.java
@@ -0,0 +1,26 @@
+package br.com.mtbassi.simpleexamplestrategypattern.strategy.impl;
+
+import br.com.mtbassi.simpleexamplestrategypattern.domain.Response;
+import br.com.mtbassi.simpleexamplestrategypattern.domain.SeniorityEnum;
+import br.com.mtbassi.simpleexamplestrategypattern.strategy.SeniorityStrategy;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class SeniorityTechLeadStrategy implements SeniorityStrategy {
+    @Override
+    public boolean matchBySeniority(SeniorityEnum seniorityEnum) {
+        return SeniorityEnum.TECH_LEAD.equals(seniorityEnum);
+    }
+
+    @Override
+    public Response calculateSalaryBonus(BigDecimal salary) {
+        var salaryIncrease = salary.add(SeniorityEnum.TECH_LEAD.calculateSalaryBonus(salary));
+        return Response.builder()
+                .bonusPercentage(SeniorityEnum.calculateBonusPercentage(salary, salaryIncrease))
+                .previousSalary(formatValue(salary))
+                .updatedSalary(formatValue(salaryIncrease))
+                .build();
+    }
+}
