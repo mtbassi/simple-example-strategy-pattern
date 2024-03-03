633074eb6b68609e42adca5eda1a4d9537866b8f
Matheus Bassi <bassi.matheus@outlook.com>
2024-03-03 19:32:18 -0300

    feat: add builder to request class

--- src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/Request.java
+++ src/main/java/br/com/mtbassi/simpleexamplestrategypattern/domain/Request.java
@@ -2,11 +2,13 @@ package br.com.mtbassi.simpleexamplestrategypattern.domain;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class Request {
 
     @Positive
